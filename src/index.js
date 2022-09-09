640013364d3f62ac5d5a7bc221836e77f2ebd2c5
Oluyemi Paul <oluola96@gmail,com>
2022-09-09 05:35:27 +0100

    Fix: remove CRA boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './components/Calculator';
-import './App.css';
-import './index.css';
+import App from './App';
+// import './App.css';
 
 // Render the Calculator to the Web page.
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
